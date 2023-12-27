6d8d03197f2f8542af1e4761fb73a765e664c6f0
v-pamela <65740938+v-pamela@users.noreply.github.com>
2023-12-27 20:42:25 -0300

    segunda entrega

--- src/app.jsx
+++ src/app.jsx
@@ -2,41 +2,24 @@ import './App.css'
 
 import NavBar from './components/NavBar/NavBar'
 import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import CardUser from './components/CardUser/CardUser'
+import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
+import { BrowserRouter, Routes , Route} from 'react-router-dom'
+import swal from 'sweetalert'
 
 function App() {
 
+  swal("Consideraciones generales:" , "El CSS no está optimizado para que me sea más simple ver las clases. Este no es el estado final. Falta vincular el terminar compra con el carrito y armar la versión final de carrito. Los botones del NavBar son diferentes porque estoy probando cuál me interesa usar. El título estará vinculado a una prop a futuro");
   return ( 
-    <div className='App'>    
-
-      <NavBar/>
-      <ItemListContainer greeting="Vite+MUI+Css personalizado+NB"/>
-      <div className='CardContainer'> 
-      <div className='userCard'>    
-        <CardUser
-        name="Ahora voy bien"
-        date="14/09/2023"
-        description="Esta es una card de prueba"
-        img="https://th.bing.com/th/id/OIP.iBd9dMuqahapIY1A_vAbiAHaJS?w=134&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-        />
-      </div>
-      <div className='userCard'>    
-        <CardUser
-        name="Acá irían los productos"
-        date="14/12/2023"
-        description="Iría la dexcripción del producto"
-        img="https://th.bing.com/th/id/OIP.iBd9dMuqahapIY1A_vAbiAHaJS?w=134&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-        />
-      </div>
-      <div className='userCard'>    
-        <CardUser
-        name="Cómo modifico más"
-        date="1/03/2023"
-        description="Ver como mejorar esto"
-        img="https://th.bing.com/th/id/OIP.iBd9dMuqahapIY1A_vAbiAHaJS?w=134&h=180&c=7&r=0&o=5&dpr=1.3&pid=1.7"
-        />
-      </div>
-      </div>
+    <div className='App'>  
+      <BrowserRouter>
+         <NavBar/>
+          <Routes>
+            <Route  path='/' element={<ItemListContainer />}/>
+            <Route  path='/category/:categoryId' element={<ItemListContainer />}/>
+            <Route  path='/item/:itemId' element={<ItemDetailContainer />}/>
+            <Route  path='*' element={<h1>404 Not found</h1>}/>
+          </Routes>
+      </BrowserRouter>
     </div>  
   )
 }
