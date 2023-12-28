67dce29692857affb57f0eb713cce78bb699809f
v-pamela <65740938+v-pamela@users.noreply.github.com>
2023-12-27 21:03:58 -0300

    segunda entrga

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,8 +1,35 @@
+import { useState } from 'react'
 import './ItemListContainer.css'
+import { useEffect } from 'react'
+import ItemList from '../ItemList/ItemList'
+import { getProducts, getProductsByCategory } from '../../asyncMock'
+import { useParams } from 'react-router-dom'
 
-const ItemListContainer = (props) => {
+
+const ItemListContainer = ({greeting}) => {
+
+  const [products, setProducts] = useState([])
+
+  const { categoryId } = useParams()
+
+  useEffect(() =>{
+
+    const asyncFunc = categoryId ? getProductsByCategory : getProducts
+    
+    asyncFunc(categoryId)
+      .then(response => {
+        setProducts(response)
+      })
+      .catch(error => {
+        console.log(error)
+      })
+  }, [categoryId])
   return (
-    <div className="ItemListContainer">{props.greeting}</div>
+    <div className='ItemListContainer'>
+      {/* aca falta configurar greeting */}
+      <h1>{greeting} BIENVENIDOS</h1> 
+      <ItemList products={products}/>
+    </div>
   )
 }
 
