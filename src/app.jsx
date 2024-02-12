b49bcdbf7947ef73e1e474a9959d078e63f9417b
v-pamela <65740938+v-pamela@users.noreply.github.com>
2024-02-12 00:01:54 -0300

    cambiando app

--- src/app.jsx
+++ src/app.jsx
@@ -1,27 +1,19 @@
-import './App.css'
-
-import NavBar from './components/NavBar/NavBar'
-import ItemListContainer from './components/ItemListContainer/ItemListContainer'
-import ItemDetailContainer from './components/ItemDetailContainer/ItemDetailContainer'
-import { BrowserRouter, Routes , Route} from 'react-router-dom'
-import swal from 'sweetalert'
+import ItemListContainer from "./components/pages/itemListContainer/ItemListContainer";
+import ItemDetailContainer from "./components/pages/itemDetailContainer/ItemDetailContainer";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import CartContainer from "./components/pages/cart/CartContainer";
+import Layout from "./components/layout/Layout";
+import AppRouter from "./router/AppRouter";
+import CartContextComponent from "./context/CartContext";
 
 function App() {
-
-  swal("Consideraciones generales:" , "El CSS no está optimizado para que me sea más simple ver las clases. Este no es el estado final. Falta vincular el terminar compra con el carrito y armar la versión final de carrito. Los botones del NavBar son diferentes porque estoy probando cuál me interesa usar. El título estará vinculado a una prop a futuro");
-  return ( 
-    <div className='App'>  
-      <BrowserRouter>
-         <NavBar/>
-          <Routes>
-            <Route  path='/' element={<ItemListContainer />}/>
-            <Route  path='/category/:categoryId' element={<ItemListContainer />}/>
-            <Route  path='/item/:itemId' element={<ItemDetailContainer />}/>
-            <Route  path='*' element={<h1>404 Not found</h1>}/>
-          </Routes>
-      </BrowserRouter>
-    </div>  
-  )
+  return (
+    <BrowserRouter>
+      <CartContextComponent>
+        <AppRouter />
+      </CartContextComponent>
+    </BrowserRouter>
+  );
 }
 
-export default App
+export default App;
