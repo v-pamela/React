e51dd7dfa9b40d27b2c1419f8970e6e7c1e42691
v-pamela <65740938+v-pamela@users.noreply.github.com>
2024-02-12 00:05:50 -0300

    cambiando

---
+++ src/productsMock.js
@@ -0,0 +1,42 @@
+export let products = [
+  {
+    title: "Top Shine Sequins",
+    price: 60000,
+    stock: 4,
+    description:
+      "Top bretelero corto con escote en V. Full bordado en mostacillas, piedras y canutillos dorados.",
+    category: "remera",
+    img: "https://www.rapsodia.com.ar/media/catalog/product/8/9/89673_rapsodia_11114924807i_38_2.jpg?quality=100&fit=bounds&height=1500&width=1000",
+    color: "Negro",
+  },
+  {
+    title: "Babucha Polis",
+    price: 35000,
+    stock: 11,
+    description:
+      "Pantalón de cuerina tipo babucha largo hasta los tobillos. Cintura con elástico.",
+    category: "pantalon",
+    img: "https://www.rapsodia.com.ar/media/catalog/product/9/0/90764_rapsodia_11114922768a_01_1.jpg?quality=100&fit=bounds&height=1500&width=1000",
+    color: "Negro",
+  },
+  {
+    title: "Short Gala",
+    price: 30000,
+    stock: 20,
+    description:
+      "Short de corte holgado tiro alto con cintura elástica y corte al ruedo. Confeccionado en cuerina.",
+    category: "pantalon",
+    img: "https://www.rapsodia.com.ar/media/catalog/product/8/9/89699_rapsodia_11114922149a_01_2.jpg?quality=100&fit=bounds&height=1500&width=1000",
+    color: "Negro",
+  },
+  {
+    title: "Remera Anne",
+    price: 10000,
+    stock: 10,
+    description:
+      "Remera manga corta confeccionada en crepe de algodón, con moldería boxy y detalle de volados en las mangas.",
+    category: "remera",
+    img: "https://www.rapsodia.com.ar/media/catalog/product/9/1/91667_rapsodia_11114924918a_01_2.jpg?quality=100&fit=bounds&height=1500&width=1000",
+    color: "Negro",
+  },
+];
